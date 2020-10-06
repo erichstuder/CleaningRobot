1c303b9f28805f1e59e51c168b58dff2d119b694
erichstuder <richu_@hotmail.com>
2020-10-06 19:33:43 +0200

    implemented blinking example

--- python/remoteControl.py
+++ python/remoteControl.py
@@ -1,10 +1,22 @@
 from getkey import getkey, keys
+import time
+import serial
 
 def main():
     print("letse goo")
+    mySerial = serial.Serial(None)
+    mySerial.timeout = 0
+    mySerial.port = "/dev/ttyACM0"
+    mySerial.open()
+    while not mySerial.is_open:
+        pass
     while True:
-        if getkey() == 't':
-            print('W')
+        #if getkey() == 't':
+        #    print('W')
+        mySerial.write("setLedOn\r".encode())
+        time.sleep(0.1)
+        mySerial.write("setLedOff\r".encode())
+        time.sleep(0.1)
 
 if __name__ == "__main__":
     main()
