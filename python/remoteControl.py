92727fa69407f4e1409aeb6a45e497736d7f6618
erichstuder <richu_@hotmail.com>
2020-11-08 17:13:21 +0100

    simple implementation to use control motors

--- python/remoteControl.py
+++ python/remoteControl.py
@@ -1,6 +1,7 @@
 from getkey import getkey, keys
 import time
 import serial
+from evdev import Device
 
 def main():
     print("letse goo")
@@ -8,15 +9,35 @@ def main():
     mySerial.timeout = 0
     mySerial.port = "/dev/ttyACM0"
     mySerial.open()
+
+    controlPad = Device("/dev/input/event5")
+
     while not mySerial.is_open:
         pass
     while True:
         #if getkey() == 't':
         #    print('W')
-        mySerial.write("setLedOn\r".encode())
-        time.sleep(0.1)
-        mySerial.write("setLedOff\r".encode())
-        time.sleep(0.1)
+
+        # the LED is on the same pin as the direction for motor B!!!
+        #mySerial.write("setLedOn\r".encode())
+        #time.sleep(0.05)
+        #mySerial.write("setLedOff\r".encode())
+        #time.sleep(0.05)
+
+        time.sleep(0.2)
+
+        controlPad.poll()
+        #print(controlPad)
+        #print(controlPad.axes)
+        axesName = "ABS_Y"
+        if axesName in controlPad.axes:
+            #print(axesName + ": " + str(controlPad.axes[axesName]))
+            mySerial.write(("setMotorA " + str(controlPad.axes[axesName]) + "\r").encode())
+        axesName = "ABS_RY"
+        if axesName in controlPad.axes:
+            #print(axesName + ": " + str(controlPad.axes[axesName]))
+            mySerial.write(("setMotorB " + str(controlPad.axes[axesName]) + "\r").encode())
+        #print("")
 
 if __name__ == "__main__":
     main()
