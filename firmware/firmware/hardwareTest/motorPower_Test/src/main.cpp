edad6e6fca71179abf2fe751b58c34929dfc6878
erichstuder <richu_@hotmail.com>
2021-03-28 21:58:28 +0200

    infrastructure to develop motor driver current

--- firmware/firmware/hardwareTest/motorPower_Test/src/main.cpp
+++ firmware/firmware/hardwareTest/motorPower_Test/src/main.cpp
@@ -1,11 +1,65 @@
 #include <Arduino.h>
 
+#include "itHandler.h"
 #include "motorPower.h"
+#include "motorInterface.h"
+#include "motorDriverCurrent.h"
 
-void setup() {
-  setMotorPower_GainI(1);
+static inline unsigned long getMicros(void);
+
+//TODO: was passiert wenn beim setzen der Getter NULL ist?
+static ItSignal_t itSignals[] = {
+	{
+		"directRatioMotorA",
+		ItValueType_Float,
+		NULL,
+		(void (*)(void)) setMotorInterface_A
+	},
+	{
+		"directRatioMotorB",
+		ItValueType_Float,
+		NULL,
+		(void (*)(void)) setMotorInterface_B
+	},
+	{
+		"controlledRatioMotorA",
+		ItValueType_Float,
+		NULL,
+		(void (*)(void)) setMotorPower_B
+	},
+	{
+		"controlledRatioMotorB",
+		ItValueType_Float,
+		NULL,
+		(void (*)(void)) setMotorPower_B
+	}
+	{
+		"driverCurrentA",
+		ItValueType_Float,
+		NULL,
+		(void (*)(void)) setMotorPower_B
+	}
+};
+
+static const unsigned char ItSignalCount = sizeof(itSignals) / sizeof(itSignals[0]);
+
+static unsigned long currentMicros;
+static unsigned long lastMicros;
+
+void setup(void) {
+	lastMicros = micros();
+	itHandlerInit(getMicros, itSignals, ItSignalCount);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(void) {
+	currentMicros = micros();
+	if(currentMicros - lastMicros < 0.01e6){
+		return;
+	}
+	lastMicros = currentMicros;
+	itHandlerTick();
+}
+
+static unsigned long getMicros(void){
+	return currentMicros;
 }
