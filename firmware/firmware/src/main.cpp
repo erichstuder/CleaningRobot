012374cc88cd900920b3af42d8eccd5b0f691de5
erichstuder <richu_@hotmail.com>
2021-09-09 11:05:26 +0200

    simplified for current usage

--- firmware/firmware/src/main.cpp
+++ firmware/firmware/src/main.cpp
@@ -1,310 +1,48 @@
 #include <Arduino.h>
-#include <stdio.h>
-#include <string.h>
 
 #include "itHandler.h"
-#include "quadratureEncoder.h"
-#include "motorPower.h"
-#include "motorSpeed.h"
 #include "speedController.h"
+#include "motorSpeed.h"
 
-
-
-// static inline void handleInput(void);
-// static inline bool charArrayBeginsWith(const char* array, const char* begin);
-// static inline void setLedOn(void);
-// static inline void setLedOff(void);
-// static inline void initLed(void);
-// static inline void appendToInputBuffer(char ch);
-static inline unsigned long getSampleTimeMicros(void);
-static inline void setSampleTimeMicros(unsigned long);
-static inline void timerSetup(unsigned long sampleTime_us);
-static inline unsigned long getTickMicros(void);
-
-
-
-static inline void setBuiltinLedOn(void);
-static inline void setBuiltinLedOff(void);
-
-//static inline uint8_t getExternalMode(void);
-static inline void setExternalMode(uint8_t value);
-
-//static inline float getBothPwm(void);
-static inline void setBothPwm(float value);
-
-//static inline float getSpeed_1(void);
-//static inline float getSpeed_2(void);
-
-//static inline void setDesiredSpeed(float value);
-
-
-
-
-static bool timerEvent = false;
-static unsigned long sampleTimeMicros;
-static unsigned long tickMicros = 0;
-static unsigned long oldTickMicros = 0;
-static float deltaTime;
-
-static const unsigned char OnBoardLedPin = 13;
-
-static bool externalMode = false;
-
-static float bothPwm;
-static float speed_1;
-static float speed_2;
-
-static float speedSetValue = 0;
-
-// static const char Terminator = '\r';
-// static char inputBuffer[100];
-// static unsigned char inputBufferIndex;
+static inline unsigned long getMicros(void);
 
 static ItSignal_t itSignals[] = {
-	/*{
-		"extMode",
-		ItValueType_Uint8,
-		[]()->uint8_t{return (uint8_t)externalMode;},
-		(void (*)(void)) setExternalMode,
-	},
 	{
-		"bothPwm",
+		"speedA",
 		ItValueType_Float,
-		[]()->float{return bothPwm;},
-		(void (*)(void)) setBothPwm,
-	},*/
-	{
-		"enc1",
-		ItValueType_Ulong,
-		(void (*)(void)) quadratureEncoder_getCounts_1,
-		NULL,
+		(void (*)(void)) getMotorAngularSpeed_1,
+		(void (*)(void)) setAngularSpeed_A
 	},
 	{
-		"enc2",
-		ItValueType_Ulong,
-		(void (*)(void)) quadratureEncoder_getCounts_2,
-		NULL,
-	},
-	/*{
-		"speed1",
+		"speedB",
 		ItValueType_Float,
-		[]()->float{return speed_1;},
-		[](float value)->float{speedSetValue = value; return 0;},
-	},
-	{
-		"speed2",
-		ItValueType_Float,
-		[]()->float{return speed_2;},
-		NULL,
-	},*/
-	{
-		"i",
-		ItValueType_Float,
-		(void (*)(void)) getSpeedControl_I,
-		(void (*)(void)) setSpeedControl_I,
-	},
-	{
-		"sampleTime_us",
-		ItValueType_Ulong,
-		(void (*)(void)) getSampleTimeMicros,
-		(void (*)(void)) setSampleTimeMicros,
-	},
+		(void (*)(void)) getMotorAngularSpeed_2,
+		(void (*)(void)) setAngularSpeed_B
+	}
 };
-
-
-
 
 static const unsigned char ItSignalCount = sizeof(itSignals) / sizeof(itSignals[0]);
 
+static unsigned long currentMicros;
+static unsigned long lastMicros;
+static const unsigned long SamplingTimeMicros = 0.01e6;
 
-void setup(void) {
-	//ItCallbacks_t itCallbacks;
-	//itCallbacks.byteFromClientAvailable = byteFromUartAvailable;
-	//itCallbacks.readByteFromClient = readByteFromUart;
-	//itCallbacks.writeByteToClient = writeByteToUart;
-	//itCallbacks.getTimestamp = getTickMicros;
-	//ItParameters_t itParameters;
-	//itParameters.itInputBuffer = itInputBuffer;
-	//itParameters.itInputBufferSize = sizeof(itInputBuffer)/sizeof(itInputBuffer[0]);
-	//itParameters.itSignals = itSignals;
-	//itParameters.itSignalCount = ItSignalCount;
-	//itInit(&itParameters, &itCallbacks);
-	itHandlerInit(getTickMicros, itSignals, ItSignalCount);
-
-	sampleTimeMicros = 0.01e6;
-	timerSetup(sampleTimeMicros);
-
-	//inputBufferIndex = 0;
-	quadratureEncoder_init();
-	initMotorPower(((float)sampleTimeMicros)/1e6f);
-	initMotorAngularSpeed();
-
-	speed_1 = 0;
-	speed_2 = 0;
+void setup(void){
+	itHandlerInit(getMicros, itSignals, ItSignalCount);
+	initSpeedController(((float)SamplingTimeMicros)/1e6);
+	lastMicros = micros();
 }
 
-void loop(void) {
-	if(!timerEvent){
-		//setBuiltinLedOff();
+void loop(void){
+	currentMicros = micros();
+	if(currentMicros - lastMicros < SamplingTimeMicros){
 		return;
 	}
-	//setBuiltinLedOn();
-	timerEvent=false;
-
-	speed_1 = getMotorAngularSpeed_1(deltaTime);
-	speed_2 = getMotorAngularSpeed_2(deltaTime);
-
-	if(externalMode){
-		//nothing to do
-	}
-	else{
-		unsigned long tickTemp = getTickMicros();
-		deltaTime = (float)(tickTemp - oldTickMicros) / 1.0e6f;
-		oldTickMicros = tickTemp;
-
-		/*float controlValue = */doSpeedControl(speedSetValue, speed_1, deltaTime);
-		///setPowerMotorA(controlValue);
-	}
-
+	lastMicros = currentMicros;
+	speedControllerTick();
 	itHandlerTick();
-
-	/*int incomingInt = Serial.read();
-	char incomingChar = (char)incomingInt;
-	if(incomingChar == Terminator) {
-		appendToInputBuffer('\0');
-		handleInput();
-	}
-	else if(incomingInt != -1) {
-		appendToInputBuffer(incomingChar);
-	}*/
 }
 
-static inline void setExternalMode(uint8_t value){
-	externalMode = (bool)value;
-	if(externalMode){
-		///setPowerMotorA(0);
-		///setPowerMotorB(0);
-	}
+static unsigned long getMicros(void){
+	return currentMicros;
 }
-
-static inline void setBothPwm(float value){
-	///setPowerMotorA(value);
-	///setPowerMotorB(value);
-	bothPwm = value;
-}
-
-static inline unsigned long getTickMicros(void){
-	return tickMicros;
-}
-
-static inline void initBuiltinLed(void){
-	pinMode(OnBoardLedPin, OUTPUT);
-}
-
-static inline void setBuiltinLedOn(void){
-	initBuiltinLed();
-	digitalWrite(OnBoardLedPin, HIGH);
-}
-
-static inline void setBuiltinLedOff(void){
-	initBuiltinLed();
-	digitalWrite(OnBoardLedPin, LOW);
-}
-
-static inline unsigned long getSampleTimeMicros(void){
-	return sampleTimeMicros;
-}
-
-static inline void setSampleTimeMicros(unsigned long sampleTime_us){
-	sampleTimeMicros = sampleTime_us;
-	timerSetup(sampleTimeMicros);
-}
-
-static inline void timerSetup(unsigned long periodMicros){
-	const unsigned long CLOCK_FREQUENCY = 16e6;
-	const unsigned long COUNTER_MAXIMUM = 65535;
-
-	TCCR1A = 0; //for any reason, this must be done!!
-
-	unsigned long counts = (CLOCK_FREQUENCY / 1e6) * periodMicros;
-	if(counts <= COUNTER_MAXIMUM){
-		TCCR1B = _BV(WGM12) | _BV(CS10); //match on value of OCR1A and divide clock by 1
-		OCR1A = counts;
-	}
-	else if((counts / 8) <= COUNTER_MAXIMUM){
-		TCCR1B = _BV(WGM12) | _BV(CS11); //match on value of OCR1A and divide clock by 8
-		OCR1A = counts / 8;
-	}
-	else if((counts / 64) <= COUNTER_MAXIMUM){
-		TCCR1B = _BV(WGM12) | _BV(CS11) | _BV(CS10); //match on value of OCR1A and divide clock by 64
-		OCR1A = counts / 64;
-	}
-	else if((counts / 256) <= COUNTER_MAXIMUM){
-		TCCR1B = _BV(WGM12) | _BV(CS12); //match on value of OCR1A and divide clock by 256
-		OCR1A = counts / 256;
-	}
-	else if((counts / 1024) <= COUNTER_MAXIMUM){
-		TCCR1B = _BV(WGM12) | _BV(CS12) | _BV(CS10); //match on value of OCR1A and divide clock by 1024
-		OCR1A = counts / 1024;
-	}
-	else{ //maximum sampletime is 4.1942s
-		TCCR1B = _BV(WGM12) | _BV(CS12) | _BV(CS10); //match on value of OCR1A and divide clock by 1024
-		OCR1A = COUNTER_MAXIMUM;
-    	}
-  
-	TIMSK1 = _BV(OCIE1A); //enable interrupt
-}
-
-ISR(TIMER1_COMPA_vect){
-	tickMicros += sampleTimeMicros;
-	timerEvent = true;
-}
-
-
-
-// static inline void handleInput(void) {
-// 	if(strcmp(inputBuffer, "setLedOn") == 0){
-// 		setLedOn();
-// 	}
-// 	else if(strcmp(inputBuffer, "setLedOff") == 0){
-// 		setLedOff();
-// 	}
-// 	else if(charArrayBeginsWith(inputBuffer, "setMotorA ")){
-// 		float ratio = atof(strstr(inputBuffer, " ") + 1);
-// 		setMotorA(ratio);
-// 	}
-// 	else if(charArrayBeginsWith(inputBuffer, "setMotorB ")){
-// 		float ratio = atof(strstr(inputBuffer, " ") + 1);
-// 		setMotorB(ratio);
-// 	}
-// 	/*else if(strcmp(inputBuffer, "getAdc_A0") == 0){
-// 		char numberString[4+sizeof('\0')];
-// 		sprintf(numberString, "%d", analogRead(A0)); //TODO: sprintf hat einen Rückgabewert!
-// 		Serial.write(numberString);
-// 		Serial.write('\n');
-// 	}*/
-// 	inputBufferIndex = 0;
-// }
-
-//static inline bool charArrayBeginsWith(const char* array, const char* begin) {
-//	return strstr(array, begin) == array;
-//}
-
-// static inline void setLedOn(void) {
-// 	initLed();
-// 	digitalWrite(LED_BUILTIN, HIGH);
-// }
-
-// static inline void setLedOff(void) {
-// 	initLed();
-// 	digitalWrite(LED_BUILTIN, LOW);
-// }
-
-// static inline void initLed(void) {
-// 	pinMode(LED_BUILTIN, OUTPUT);
-// }
-
-// static inline void appendToInputBuffer(char ch) {
-// 	inputBuffer[inputBufferIndex] = ch;
-// 	inputBufferIndex++;
-// }
